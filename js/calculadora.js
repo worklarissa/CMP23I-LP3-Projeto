8beedb38c817587e19397b7b5890e06123764b70
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-02-17 17:23:09 -0300

    terminei o projeto

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,45 +3,51 @@ function somar() {
   const numero2 = parseFloat(document.getElementById("numero2Soma").value);
   // evite mexer no código acima!
   console.log(numero1)
-  //valorSoma = numero1 + numero2;
+
+  if (numero1 == 0 || numero2 == NaN ) {
+    document.getElementById("resultadoSoma").innerHTML = alert("É necessário 2 valores para realizar a soma"); 
+  }
+  
+  const soma = numero1 + numero2;
 
 
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
   Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
   
-  document.getElementById("resultadoSoma").innerHTML = numero1  + numero2;
+  document.getElementById("resultadoSoma").innerHTML = soma;
 }
 
 function subtrair() {
   const numero1 = parseFloat(document.getElementById("numero1Sub").value);
   const numero2 = parseFloat(document.getElementById("numero2Sub").value);
   // evite mexer no código acima!
-
+  const subtrair = numero1 - numero2;
+  
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
-  document.getElementById("resultadoSub").innerHTML = numero1 - numero2;
+  document.getElementById("resultadoSub").innerHTML = subtrair;
 }
 
 function multiplicar() {
   const numero1 = parseFloat(document.getElementById("numero1Mult").value);
   const numero2 = parseFloat(document.getElementById("numero2Mult").value);
   // evite mexer no código acima!
-  
+  const vezes = numero1 * numero2;
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
-  document.getElementById("resultadoMult").innerHTML = numero1 * numero2;
+  document.getElementById("resultadoMult").innerHTML = vezes;
 }
 
 function dividir() {
   const numero1 = parseFloat(document.getElementById("numero1Div").value);
   const numero2 = parseFloat(document.getElementById("numero2Div").value);
   // evite mexer no código acima!
-  
+  const divisao = numero1 / numero2
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
-  document.getElementById("resultadoDiv").innerHTML = numero1 / numero2;
+  document.getElementById("resultadoDiv").innerHTML = divisao;
 }
--- media.js
+++ media.js
@@ -13,15 +13,34 @@
 */
 function calcularMedia() {
   const nota1 = parseFloat(document.getElementById("nota1").value);
-  const note2 = parseFloat(document.getElementById("nota2").value);
-  const note3 = parseFloat(document.getElementById("nota3").value);
-  const note4 = parseFloat(document.getElementById("nota4").value);
+  const nota2 = parseFloat(document.getElementById("nota2").value);
+  const nota3 = parseFloat(document.getElementById("nota3").value);
+  const nota4 = parseFloat(document.getElementById("nota4").value);
 
   let mensagem = "Ops! tem algo errado. Tente novamente!";
   // evite mexer no código acima!
-  switch(nota1, nota2, nota3, nota4):
-  case (null, null, null, null): break;
-  case 
+  const soma = nota1 + nota2 + nota3 + nota4
+  const media = soma / 4
+
+  if (media == 0) {
+    mensagem = "Infelizmente você zerou a prova. :("
+  }
+  else if (media > 0.1 && media <= 3){
+    mensagem = "Caramba, deu ruim, você obteve media " + media + "! Estude mais e tente novamente."
+  }
+  else if (media > 3.1 &&  media <= 5.9 ) {
+    mensagem = "Você obteve media " + media + "! Falta pouco para a média."
+  }
+  else if (media >= 6 && media <= 7 ){
+    mensagem = "Você está na média com" + media
+  }
+  else if  (media >= 7.1 && media <= 9.9) {
+    mensagem = "Notão! Sua média é " + media
+  }
+  else if (media == 10) {
+    mensagem = "Hoje é seu aniversário? Pq você ta de parabéns! 10 de média"
+  }
+
   // evite mexer na linha de código abaixo!
   document.getElementById("situacaoAluno").innerHTML = mensagem;
 }
